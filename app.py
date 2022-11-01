40d7a2a22d55db8141071e6999a6fb0a1b7e85a2
Mike Martley <martleym@gmail.com>
2022-11-01 16:25:12 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from data.data import data_prep
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+
+data = data_prep()
+options = list(zip(data.member_images['name'], data.member_images['email'], data.member_images['imageURL']))
+col1, col2, col3 = st.columns([.4, .2, .4])
+col1.markdown('<h3 style="text-align:center;color:black;font-weight:bolder;font-size:70px;">Member Images</h3>',unsafe_allow_html=True)
+option = col1.selectbox("Select a Token Recipient",options)
+col1.image(option[2])
+
+options = list(data.images["collection"].unique())
+col3.markdown('<h3 style="text-align:center;color:black;font-weight:bolder;font-size:70px;">Image Collections</h3>',unsafe_allow_html=True)
+option = col3.selectbox("Select Image Collection",options)
+token_data = data.images[data.images["collection"]==option]
+pie_data = token_data["minted"].value_counts()
+col3.write(px.pie(pie_data, values=pie_data, names=pie_data.index, width=400, height=400))
+
+archived_members = data.members[data.members['status']=='2']
+new_members = data.members[data.members['status']=='1']
+members_with_tokens = data.members[data.members['status']=='3']
+st.markdown('<h1 style="text-align:center;color:black;font-weight:bolder;font-size:70px;">Membership</h1>',unsafe_allow_html=True)
+col1, col2, col3 = st.columns([.3, .3, .3])
+col1.metric("Archived Members", len(archived_members))
+col2.metric("Members with Tokens", len(members_with_tokens))
+col3.metric("New Members Active (Under 60 days)", len(new_members))
+
+new_members = data.members[~data.members["dtInsert"].isnull()]
+new_members = new_members.groupby(by="dtInsert").size().reset_index(name="counts")
+new_members = new_members.sort_values(by=["dtInsert"], ascending=True)
+new_members['dtInsert'] = pd.to_datetime(new_members['dtInsert']).dt.strftime('%d/%m/%Y')
+hist = px.bar(data_frame=new_members, x="dtInsert", y="counts", barmode="group",
+ labels={"counts": "# New Members", 'dtInsert': "Date Joined"})
+st.write(hist)
---
+++ data/data.py
@@ -0,0 +1,82 @@
+import azure.cosmos.cosmos_client as cosmos_client
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+CONFIG = {
+    "ENDPOINT": st.secrets['ENDPOINT'],
+    "KEY": st.secrets['KEY'],
+    "DATABASE": st.secrets['DATABASE'],
+    "CONT1": st.secrets['CONT1'],
+    "CONT2": st.secrets['CONT2'],
+    "CONT3": st.secrets['CONT3']
+}
+
+class CosmosData:
+    def __init__(self, url, credential, database):
+        self.url = url
+        self.credential = credential
+        self.database = database
+        self.client = None
+        self.db = None
+    
+    def setup_client(self):
+        try:
+            self.client = cosmos_client.CosmosClient(self.url, self.credential)
+        except:
+            raise ConnectionError("Error creating the client")
+
+    def setup_db(self):
+        try:
+            self.db = self.client.get_database_client(self.database)
+        except:
+            raise ConnectionError("Error establishing database connection")
+
+    def get_container(self,container_id):
+        try:
+            container = self.db.get_container_client(container_id)
+            return container
+        except:
+            raise ConnectionError("Error connecting to container")
+
+    def get_container_data(self,container, query):
+        df_list = []
+        try:
+            for item in container.query_items(query=query, enable_cross_partition_query=True):
+                df_list.append(dict(item))
+        except: 
+            raise ConnectionError("Error querying database container")
+        if len(df_list) > 0:
+            return df_list
+        else:
+            return None    
+
+def is_minted(x):
+    if x == 'None': 
+        return "Unused"
+    else:
+        return "Used"
+
+@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+def data_prep():
+    data = CosmosData(CONFIG["ENDPOINT"], CONFIG["KEY"], CONFIG["DATABASE"])
+    data.setup_client()
+    data.setup_db()
+    data.container1 = data.get_container(CONFIG["CONT1"]) 
+    data.container2 = data.get_container(CONFIG["CONT2"])
+    data.container3 = data.get_container(CONFIG["CONT3"])
+    data.transactions_raw = data.get_container_data(data.container1, 'SELECT * FROM c')
+    data.images_raw = data.get_container_data(data.container2, 'SELECT * FROM c')
+    data.members_raw = data.get_container_data(data.container3, 'SELECT * FROM c')
+
+    df_images = pd.DataFrame(data.images_raw)
+    df_images = pd.concat([df_images, pd.DataFrame(list(df_images['keyvalues']))], axis=1)
+    images = df_images.loc[:, df_images.columns.isin(["imageURL","uniqueId","collection"])]
+    images['uniqueId'].fillna('None', inplace=True)
+    images['minted'] = images['uniqueId'].apply(is_minted)
+    data.images = images
+    df_transactions = pd.DataFrame(data.transactions_raw)
+    data.transactions_filtered = df_transactions.loc[:, df_transactions.columns.isin(["name","email","dtInsert","appResponse","dtUpdated","uniqueId"])]
+    data.member_images = data.transactions_filtered.merge(images[['imageURL', 'uniqueId']], how='left', on='uniqueId')
+    data.members = pd.DataFrame(data.members_raw)
+    return data
